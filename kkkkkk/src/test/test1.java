d11e855254e7d59ed635faf619d40bea9784f687
tanhongwang <1774693441@qq.com>
2017-10-14 12:01:18 +0800

    SaaS

--- kkkkkk/src/test/test1.java
+++ kkkkkk/src/test/test1.java
@@ -7,7 +7,7 @@ import util.MybatisUtil;
 public class test1 {
 	public static void main(String[] args) {
 		StudentDao dao = (StudentDao) MybatisUtil.getMapper(StudentDao.class);
-//		Students stu=dao.querybyid(2);
-//		System.out.println(stu);
+		Students stu=dao.querybyid(2);
+		System.out.println(stu);
 	}
 }
